2ac33dc0e856f96a842c33b36b91c7c056b2ddbb
nabil-dee <nabilasa921@gmail.com>
2025-03-18 00:41:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById("shortlinkForm").addEventListener("submit", function(eve
     
     const originalUrl = document.getElementById("originalUrl").value;
     const customSlug = document.getElementById("customSlug").value;
-    let shortlink = "https://shortlink-nabila.netlify.app/" + (customSlug || Math.random().toString(36).substr(2, 5));
+    let shortlink = "http://erabox.unaux.com/" + (customSlug || Math.random().toString(36).substr(2, 5));
 
     document.getElementById("shortlinkResult").innerHTML = `<strong>Shortlink:</strong> <a href="${shortlink}" target="_blank">${shortlink}</a>`;
 });
